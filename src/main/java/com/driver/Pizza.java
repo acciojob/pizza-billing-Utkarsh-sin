ddba0974572a4a66f33c2adf239b039d9408b70d
Utkarsh-sin <sinhautkarsh38@gmail.com>
2023-01-21 19:54:37 +0530

    Pizza

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,9 +6,15 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
+    private int takeAwayCount=0;
+    private int addTopping =0;
+    private int addCheese =0;
+
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg==true) price=300;
+        else price =400;
     }
 
     public int getPrice(){
@@ -17,18 +23,52 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(this.getClass().equals(DeluxePizza.class)==false) {
+            addCheese++;
+            if (addCheese == 1)
+                price = price + 80;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(this.getClass().equals(DeluxePizza.class)==false) {
+            addTopping++;
+            if (isVeg == true && addTopping == 1) {
+                price = price + 70;
+            } else if (addTopping == 1) {
+                price = price + 120;
+            }
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        takeAwayCount++;
+        if(takeAwayCount==1)
+            price=price+20;
     }
 
     public String getBill(){
         // your code goes here
+        boolean DP=false;
+        if(this.getClass().equals(DeluxePizza.class)){
+            DP=true;
+        }
+        if(isVeg) {
+            bill = "Base Price Of The Pizza: 300 \n";
+            if(addCheese>=1 && DP==false) bill+="Extra Cheese Added: 80 \n";
+            if(addTopping>=1 && DP==false) bill+="Extra Toppings Added: 70 \n";
+            if(takeAwayCount>=1) bill+="Paperbag Added: 20 \n";
+            bill+="Total Price: "+price +"\n";
+        }
+        else{
+            bill = "Base Price Of The Pizza: 400 \n";
+            if(addCheese>=1 && DP==false) bill+="Extra Cheese Added: 80 \n";
+            if(addTopping>=1 && DP==false) bill+="Extra Toppings Added: 120 \n";
+            if(takeAwayCount>=1) bill+="Paperbag Added: 20 \n";
+            bill+="Total Price: "+price;
+        }
         return this.bill;
     }
 }
